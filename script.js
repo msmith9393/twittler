09520c2f372d6760c1358d5a8b78e5ef49352d54
Megan Smith <megan@tokaypress.com>
2016-01-25 18:11:38 -0800

    Username buttons work when clicked more than once

--- script.js
+++ script.js
@@ -19,7 +19,9 @@ $(document).ready(function(){
   // when page loads should have generated 11 tweets, therefore index is 10
   var index = streams.home.length - 1;
   // start index for new tweets that are being created every few seconds
-  var startIndex = streams.home.length
+  var startIndex = streams.home.length;
+
+  $('.back-btn').hide();
 
   while(index >= 0){
     var tweet = streams.home[index];
@@ -27,25 +29,28 @@ $(document).ready(function(){
     index -= 1;
   }
 
-  $('.new-tweet-btn').on('click', function() {
+  $(document).on('click', '.new-tweet-btn', function() {
     var newTweets = streams.home.slice(startIndex);
     renderTweets(newTweets);
-    startIndex = newTweets;
+    startIndex = newTweets.length;
   });
 
-  $('.username').on('click', function(event) {
+  $(document).on('click', 'a', function(event) {
     var username = event.currentTarget.innerHTML.slice(1);
-    console.log(username);
+    if ($('.usernameTitle').length === 0) {
+      var usernameTitle = $('<h2 class="usernameTitle"> Tweets by: ' + username + '</h2>');
+      $('header').append(usernameTitle);
+    }
     $('.new-tweet-btn').hide();
+    $('.back-btn').show();
     $('.tweet-feed').empty();
     var usersTweets = streams.users[username];
     renderTweets(usersTweets);
-
-    $('<button class="back-btn">back to all tweets</button>').insertBefore($('.tweet-feed'))
     
-    $('.back-btn').on('click', function(event) {
+    $(document).on('click', '.back-btn', function(event) {
       var allTweets = streams.home;
       $('.new-tweet-btn').show();
+      $('.usernameTitle').remove();
       $('.back-btn').hide();
       $('.tweet-feed').empty();
       renderTweets(allTweets);
@@ -65,7 +70,7 @@ $(document).ready(function(){
     var $tweet = $('<div class="tweet"></div>');
     $tweet.append('<p><a href="#" class="username">@' + tweet.user + '</a> &bull; <span class="date">' + tweet.created_at + '</span></p>')
     .append('<p>' + messageArr[0] + '<span class="hashtag">' + messageArr[1] + '</span></p>')
-    .prependTo($('.tweet-feed'))
+    .prependTo($('.tweet-feed'));
   };
 
   function renderTweets(newTweets) {
